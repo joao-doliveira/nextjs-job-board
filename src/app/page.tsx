34938fe0f58a000ca2988f6bf57d49af8d8bcd56
joaodoliveira-wolven <joao.doliveira@wolven.com.br>
2024-04-19 09:04:53 -0300

    feat: JobListItem and JobListSidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import JobFilterSidebar from "@/components/JobFilterSidebar";
+import { JobListItem } from "@/components/JobListItem";
 import prisma from "@/lib/prisma";
 export default async function Home() {
   const jobs = await prisma.job.findMany({
@@ -7,5 +9,22 @@ export default async function Home() {
     orderBy: { createdAt: "desc" },
   });
 
-  return <main>{JSON.stringify(jobs)}</main>;
+  return (
+    <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
+      <div className="space-y-5 text-center">
+        <h1 className="lg:text-5x1 text-4xl font-extrabold tracking-tight">
+          Developer Jobs
+        </h1>
+        <p className="text-muted-foreground">Find your dream job.</p>
+      </div>
+      <section className="flex flex-col md:flex-row gap-4">
+        <JobFilterSidebar />
+        <div className="space-y-4 grow">
+          {jobs.map((job) => (
+            <JobListItem job={job} key={job.id} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 }
---
+++ src/components/JobFilterSidebar.tsx
@@ -0,0 +1,44 @@
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import Select from "./ui/select";
+import prisma from "@/lib/prisma";
+
+async function filterJobs(formData: FormData) {
+  "use server";
+}
+
+export default async function JobFilterSidebar() {
+  const distinctLocations = (await prisma.job
+    .findMany({
+      where: { approved: true },
+      select: { location: true },
+      distinct: ["location"],
+    })
+    .then((locations) =>
+      locations.map(({ location }) => location).filter(Boolean),
+    )) as string[];
+
+  return (
+    <aside className="sticky top-0 h-fit rounded-lg border bg-background p-4 md:w-[260px]">
+      <form action={filterJobs}>
+        <div className="space-y-4">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="q">Search</Label>
+            <Input id="q" name="q" placeholder="Title, company, etc.. " />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="location">Location</Label>
+            <Select id="location" name="location" defaultValue="">
+              <option value="">All locations</option>
+              {distinctLocations.map((location) => (
+                <option key={location} value={location}>
+                  {location}
+                </option>
+              ))}
+            </Select>
+          </div>
+        </div>
+      </form>
+    </aside>
+  );
+}
